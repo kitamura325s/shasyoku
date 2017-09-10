fa444d409b73a6856dcf303b27a0502c32a34f96
Kitamura Saori <kitamura325s@gmail.com>
2017-09-10 03:53:31 +0000

    first commit

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,52 @@
+class PicturesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_picture, only: [:edit, :update, :destroy]
+  
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    if params[:back]
+      @picture = Picture.new(pictures_params)
+    else
+      @picture = Picture.new
+    end
+  end
+
+  def create
+    @picture = Picture.new(pictures_params)
+    @picture.user_id = current_user.id
+    if @picture.save
+      redirect_to pictures_path, notice: "写真を投稿しました！"
+      #NoticeMailer.sendmail_picture(@picture).deliver
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @picture.update(pictures_params)
+      redirect_to pictures_path, notice: "投稿を編集しました！"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, notice: "投稿を削除しました！"
+  end
+  
+  private
+    def pictures_params
+      params.require(:picture).permit(:content)
+    end
+    
+    def set_picture
+      @picture = Picture.find(params[:id])
+    end    
+end
